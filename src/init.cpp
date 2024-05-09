c4a389f70a2df36bc07ad8f35d04b0094b78ca56
Nevemlaci <nevemlaci@proton.me>
2024-05-09 15:48:44 +0200

    image changes

--- src/init.cpp
+++ src/init.cpp
@@ -6,16 +6,21 @@ namespace SGL2 {
 	Game* init() {
 		Game* game = new Game("title");
 #ifndef CPORTA
-		game->GetRenderer().SetRenderDrawColor(0, 100, 140, 255); //manually get an SDL component from the game
-		game->GetAssetManager().AddTexture("charactertexture", "test.png"); //load an asset using the asset manager
-		game->GetAssetManager().AddTexture("background", "bg.png");
-		game->GetAssetManager().AddTexture("dummytexture", "dummy.png");
+		//manual SDL access example
+		game->GetRenderer().SetRenderDrawColor(0, 100, 140, 255); 
+		//load assets using the asset manager
+		game->GetAssetManager().AddTexture("charactertexture", "player.png");
+		game->GetAssetManager().AddTexture("background", "background.jpeg");
+		game->GetAssetManager().AddTexture("dummytexture", "meteor.png");
 #endif
-		g1& g = game->AddGameObject<g1>("g1"); //Add the gameobject and get a reference to it from the game	 (for testing)
+		//Add a dummy game object for unit testing 
+		g1& g = game->AddGameObject<g1>("g1");
 
-		g.GetComponent<c1>();
+		//this is here to avoid unused variable warnings xd
+		g.GetComponent<c1>(); 
 
 #ifndef CPORTA
+		//add bunch of test objects
 		game->AddGameObject<background>("testbackgroundobject");
 		game->AddGameObject<dummy>("dummy1", Vector2(100, 400));
 		game->AddGameObject<dummy>("dummy2", Vector2(400, 800), Vector2(100, 50));
